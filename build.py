317dacb0b7d5c489c6c4640d756d2de7041a753a
Luis Martinez de Bartolome <lasote@gmail.com>
2016-01-23 02:18:24 +0100

    New package tools

--- build.py
+++ build.py
@@ -4,56 +4,35 @@ import sys
 import platform
 from copy import copy
 
-def add_visual_builds(builder, visual_version, arch):
-    if visual_version == 10 and arch=="x86_64":
-        return
-        
-    base_set = {"compiler": "Visual Studio", 
-                "compiler.version": visual_version, 
-                "arch": arch}
-    sets = []
-    sets.append([{"build_type": "Release", "compiler.runtime": "MT"}, {"Boost:shared":False}])        
-    sets.append([{"build_type": "Debug", "compiler.runtime": "MTd"}, {"Boost:shared":False}])
-    sets.append([{"build_type": "Debug", "compiler.runtime": "MDd"}, {"Boost:shared":False}])
-    sets.append([{"build_type": "Release", "compiler.runtime": "MD"}, {"Boost:shared":False}])
-    sets.append([{"build_type": "Debug", "compiler.runtime": "MDd"}, {"Boost:shared":True}])
-    sets.append([{"build_type": "Release", "compiler.runtime": "MD"}, {"Boost:shared":True}])        
-      
-    for setting, options in sets:
-       tmp = copy(base_set)
-       tmp.update(setting)
-       builder.add(tmp, options)
-       
-def add_other_builds(builder):
-    # Not specified compiler or compiler version, will use the auto detected     
-    for arch in ["x86", "x86_64"]:
-        for shared in [True, False]:
-            for build_type in ["Debug", "Release"]:
-                if arch == "x86" and (platform.system() == "Darwin" or os.getenv("TRAVIS", False)):    
-                    continue
-                builder.add({"arch":arch, "build_type": build_type}, {"Boost:shared": shared})
-           
-def get_builder(username, channel):
-    args = " ".join(sys.argv[1:])
-    builder = ConanMultiPackager(args, username, channel)
-    if platform.system() == "Windows":
-        for visual_version in [10, 12, 14]:
-            for arch in ["x86", "x86_64"]:
-                add_visual_builds(builder, visual_version, arch)
-    else:
-        add_other_builds(builder)
-    
-    return builder
-        
 if __name__ == "__main__":
     channel = os.getenv("CONAN_CHANNEL", "testing")
     username = os.getenv("CONAN_USERNAME", "lasote")
     current_page = os.getenv("CONAN_CURRENT_PAGE", "1")
     total_pages = os.getenv("CONAN_TOTAL_PAGES", "1")
-    
-    builder = get_builder(username, channel)
-    builder.pack(current_page, total_pages)
-    
-    if os.getenv("CONAN_UPLOAD", False) and os.getenv("CONAN_REFERENCE") and os.getenv("CONAN_PASSWORD"):
-        reference = os.getenv("CONAN_REFERENCE")
-        builder.upload_packages(reference, os.getenv("CONAN_PASSWORD"))
+    gcc_versions = os.getenv("CONAN_GCC_VERSIONS", None)
+    gcc_versions = gcc_versions.split(",") if gcc_versions else None
+    use_docker = os.getenv("CONAN_USE_DOCKER", False)
+    upload = os.getenv("CONAN_UPLOAD", False)
+    reference = os.getenv("CONAN_REFERENCE")
+    password = os.getenv("CONAN_PASSWORD")
+    travis = os.getenv("TRAVIS", False)
+    travis_branch = os.getenv("TRAVIS_BRANCH", None)
+    appveyor = os.getenv("APPVEYOR", False)
+    appveyor_branch = os.getenv("APPVEYOR_REPO_BRANCH", None)
+
+    channel = "stable" if travis and travis_branch=="master" else channel
+    channel = "stable" if appveyor and appveyor_branch=="master" and not os.getenv("APPVEYOR_PULL_REQUEST_NUMBER") else channel
+    os.environ["CONAN_CHANNEL"] = channel # Override the environment value for test/conanfile.py file
+
+    args = " ".join(sys.argv[1:])
+    builder = ConanMultiPackager(args, username, channel)
+    builder.add_common_builds(shared_option_name="Boost:shared", visual_versions=[10, 12, 14])
+    print(builder.builds)
+
+    if use_docker:  
+        builder.docker_pack(current_page, total_pages, gcc_versions)
+    else:
+        builder.pack(current_page, total_pages)
+
+    if upload and reference and password:
+        builder.upload_packages(reference, password)
