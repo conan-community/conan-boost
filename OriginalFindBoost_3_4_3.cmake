1d114b1505a9835f7091e4bd94a02d97ea74e0df
dkgs <dkgs@users.noreply.github.com>
2016-12-13 10:18:53 +0100

    Switch back to USE_DEBUG_RUNTIME

--- OriginalFindBoost_3_4_3.cmake
+++ OriginalFindBoost_3_4_3.cmake
@@ -114,8 +114,9 @@
 #   Boost_USE_STATIC_RUNTIME - Set to ON or OFF to specify whether to use
 #                              libraries linked statically to the C++ runtime
 #                              ('s' tag).  Default is platform dependent.
-#   Boost_USE_DEBUG_TAG      - Set to ON or OFF to specify whether to use
-#                              libraries in debug mode. Default is ON.
+#   Boost_USE_DEBUG_RUNTIME  - Set to ON or OFF to specify whether to use
+#                              libraries linked to the MS debug C++ runtime
+#                              ('g' tag).  Default is ON.
 #   Boost_USE_DEBUG_PYTHON   - Set to ON to use libraries compiled with a
 #                              debug Python build ('y' tag). Default is OFF.
 #   Boost_USE_STLPORT        - Set to ON to use libraries compiled with
@@ -919,8 +920,8 @@ endif()
 if(NOT DEFINED Boost_USE_MULTITHREADED)
     set(Boost_USE_MULTITHREADED TRUE)
 endif()
-if(NOT DEFINED Boost_USE_DEBUG_TAG)
-  set(Boost_USE_DEBUG_TAG TRUE)
+if(NOT DEFINED Boost_USE_DEBUG_RUNTIME)
+  set(Boost_USE_DEBUG_RUNTIME TRUE)
 endif()
 
 # Check the version of Boost against the requested version.
@@ -1296,7 +1297,7 @@ if(Boost_USE_STATIC_RUNTIME)
 endif()
 #  g        using debug versions of the standard and runtime
 #           support libraries
-if(WIN32 AND Boost_USE_DEBUG_TAG)
+if(WIN32 AND Boost_USE_DEBUG_RUNTIME)
   if("x${CMAKE_CXX_COMPILER_ID}" STREQUAL "xMSVC"
           OR "${CMAKE_CXX_COMPILER}" MATCHES "icl"
           OR "${CMAKE_CXX_COMPILER}" MATCHES "icpc")
