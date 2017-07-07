1b3c134116b2c29fc0442e44b5ffbe1a8076e41b
Torbjörn Klatt <t.klatt.oss@mailbox.org>
2017-07-07 19:35:33 +0200

    use zlib from conan-center
    Signed-off-by: Torbjörn Klatt <t.klatt.oss@mailbox.org>

--- conanfile.py
+++ conanfile.py
@@ -110,7 +110,7 @@ class BoostConan(ConanFile):
                 self.requires("bzip2/1.0.6@lasote/stable")
                 if not self.options.header_only:
                     self.options["bzip2/1.0.6"].shared = self.options.shared
-            self.requires("zlib/1.2.11@lasote/stable")
+            self.requires("zlib/1.2.11@conan/stable")
             if not self.options.header_only:
                 self.options["zlib"].shared = self.options.shared
 
