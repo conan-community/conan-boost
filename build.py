e13a801e3616596ad7539da8fdd2933dba58b12d
Luis Martinez de Bartolome <lasote@gmail.com>
2017-08-18 10:09:06 +0200

    Fixed CI

--- build.py
+++ build.py
@@ -6,14 +6,14 @@ import platform
 if __name__ == "__main__":
     builder = ConanMultiPackager()
     builder.add_common_builds(shared_option_name="Boost:shared", pure_c=False)    
-    builder.builds.append([{}, {"Boost:header_only": True}])
+    builder.add({}, {"Boost:header_only": True}, {}, {})
     if platform.system() == "Linux":
         filtered_builds = []
-        for settings, options in builder.builds:
+        for settings, options, env_vars, build_requires in builder.builds:
             filtered_builds.append([settings, options])
             new_options = copy.copy(options)
             new_options["Boost:fPIC"] = True
-            filtered_builds.append([settings, new_options])
+            filtered_builds.append([settings, new_options, env_vars, build_requires])
         builder.builds = filtered_builds
     builder.run()
 
