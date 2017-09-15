f6ffa088d1aa0cad80bcf03590afcdf3202797cd
Luis Martinez de Bartolome <lasote@gmail.com>
2017-09-15 16:05:09 +0200

    Try now

--- build.py
+++ build.py
@@ -1,14 +1,17 @@
 from conan.packager import ConanMultiPackager
 import copy
 import platform
+import os
 
 
 if __name__ == "__main__":
     builder = ConanMultiPackager()
     builder.add_common_builds(shared_option_name="Boost:shared", pure_c=False)
-    new_build = copy.deepcopy(builder.builds)[-1]
-    new_build.options["Boost:header_only"] = True
-    builder.add(*new_build)
+    if os.getenv("CONAN_VISUAL_VERSIONS") == "15":
+        # There is an unknown problem in visual 2015 trying to use 2017 to do something with b2
+        new_build = copy.deepcopy(builder.builds)[-1]
+        new_build.options["Boost:header_only"] = True
+        builder.add(*new_build)
 
     if platform.system() == "Linux":
         filtered_builds = []
