624a3084272d3359d6c497bdea260e2be2435436
Luis Martinez de Bartolome <luism@jfrog.com>
2018-01-18 14:48:50 +0100

    better launcher

--- .launcher.py
+++ .launcher.py
@@ -7,7 +7,8 @@ from conans import tools
 from conans.client.output import ConanOutput
 
 env = {"CONAN_USERNAME": "lasote",
-       "CONAN_CHANNEL": "testing"}
+       "CONAN_CHANNEL": "testing",
+       "CONAN_REFERENCE": "Boost/1.66.0"}
 
 if platform.system() == "Windows":
     env["CONAN_VISUAL_VERSIONS"] = "15, 14, 12"
@@ -16,10 +17,12 @@ elif platform.system() == "Linux":
     env["CONAN_USE_DOCKER"] = "1"
 elif platform.system() == "Darwin":
     from conans.client.conf.detect import _get_default_compiler
-    _ , version = _get_default_compiler(ConanOutput(stream=sys.stdout))
+    _, version = _get_default_compiler(ConanOutput(stream=sys.stdout))
     env["CONAN_APPLE_CLANG_VERSIONS"] = version
 
 with tools.environment_append(env):
     with tools.chdir(os.path.dirname(os.path.realpath(__file__))):
-        os.system("python build.py")
+        ret = os.system("python build.py")
+        if ret != 0:
+            raise Exception("Error!")
 
