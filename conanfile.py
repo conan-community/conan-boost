8cd2804195b69c5e503a450bb38405e88d937dda
Luis Martinez de Bartolome <luism@jfrog.com>
2018-04-26 11:42:00 +0200

    Option to keep lib names

--- conanfile.py
+++ conanfile.py
@@ -23,11 +23,12 @@ class BoostConan(ConanFile):
     options = {
         "shared": [True, False],
         "header_only": [True, False],
-        "fPIC": [True, False]
+        "fPIC": [True, False],
+        "skip_lib_rename": [True, False]
     }
     options.update({"without_%s" % libname: [True, False] for libname in lib_list})
 
-    default_options = ["shared=False", "header_only=False", "fPIC=False"]
+    default_options = ["shared=False", "header_only=False", "fPIC=False", "skip_lib_rename=False"]
     default_options.extend(["without_%s=False" % libname for libname in lib_list if libname != "python"])
     default_options.append("without_python=True")
     default_options = tuple(default_options)
@@ -307,21 +308,22 @@ class BoostConan(ConanFile):
         self.renames_to_make_cmake_find_package_happy()
 
     def renames_to_make_cmake_find_package_happy(self):
-        # CMake findPackage help
-        renames = []
-        for libname in os.listdir(os.path.join(self.package_folder, "lib")):
-            new_name = libname
-            libpath = os.path.join(self.package_folder, "lib", libname)
-            if "-" in libname:
-                new_name = libname.split("-", 1)[0] + "." + libname.split(".")[-1]
-                if new_name.startswith("lib"):
-                    new_name = new_name[3:]
-            renames.append([libpath, os.path.join(self.package_folder, "lib", new_name)])
+        if not self.options.skip_lib_rename:
+            # CMake findPackage help
+            renames = []
+            for libname in os.listdir(os.path.join(self.package_folder, "lib")):
+                new_name = libname
+                libpath = os.path.join(self.package_folder, "lib", libname)
+                if "-" in libname:
+                    new_name = libname.split("-", 1)[0] + "." + libname.split(".")[-1]
+                    if new_name.startswith("lib"):
+                        new_name = new_name[3:]
+                renames.append([libpath, os.path.join(self.package_folder, "lib", new_name)])
 
-        for original, new in renames:
-            if original != new and not os.path.exists(new):
-                self.output.info("Rename: %s => %s" % (original, new))
-                os.rename(original, new)
+            for original, new in renames:
+                if original != new and not os.path.exists(new):
+                    self.output.info("Rename: %s => %s" % (original, new))
+                    os.rename(original, new)
 
     def package_info(self):
         gen_libs = tools.collect_libs(self)
