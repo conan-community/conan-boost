a318f36ecba0842c041ae5c19728e0d7e889acbf
SSE4 <tomskside@gmail.com>
2018-12-09 22:08:39 +0700

    - enable Clang libstdc++11 builds [skip appveyor][build=outdated]
    Signed-off-by: SSE4 <tomskside@gmail.com>

--- build.py
+++ build.py
@@ -19,11 +19,14 @@ if __name__ == "__main__":
                 # MinGW shared with linker errors. I don't have a clue
                 if settings["compiler"] == "gcc" and options["boost:shared"] is True:
                     continue
+                filtered_builds.append([settings, options, env_vars, build_requires])
+            builder.builds = filtered_builds
+        if platform.system() == "Linux":
+            for settings, options, env_vars, build_requires in builder.builds:
                 if settings["compiler"] == "clang":
                     settings_libstdcxx11 = settings.copy()
                     settings_libstdcxx11["compiler.libcxx"] = "libstdc++11"
                     filtered_builds.append([settings_libstdcxx11, options, env_vars, build_requires])
                 filtered_builds.append([settings, options, env_vars, build_requires])
             builder.builds = filtered_builds
-
     builder.run()
